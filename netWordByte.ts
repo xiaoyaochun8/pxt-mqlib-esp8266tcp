c8389014ae4b152d63e31de488e6a5504449573e
Barry.C <wjyzxhm888@163.com>
2026-06-20 09:46:10 +0800

    更新 pxt.json, netWordByte.ts

---
+++ netWordByte.ts
@@ -0,0 +1,29 @@
+/**
+ * esp8266 blocks
+ */
+//% groups=['esp8266']
+namespace mqlib {
+
+    //% subcategory="esp8266"
+    //% group='getNetTs'
+    //% block
+    export function showOledWord(ip: string, port: string): string {
+        //循环拉取数据
+        let data = 'getwordbyte,data1,0'
+        requestServerData(ip, port, data)
+        let aryRsp: AryRsp = getServerData()
+        let sRsp = ''
+        if (aryRsp.code == 0) {
+            sRsp = processData(aryRsp.data)
+            //执行显示 oled.show()
+        }
+        //暂停100ms？
+        return sRsp
+    }
+
+    function processData(inputStr: string): string {
+        let tmp = inputStr
+        return tmp
+    }
+
+}
